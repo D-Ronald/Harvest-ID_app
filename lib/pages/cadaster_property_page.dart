7aa3a8fc0436cf36214440a2e6bde16cf31db1cd
Arinesouza <souzandrade62@gmail.com>
2023-11-27 10:17:25 -0300

    Funcionalidades: -Formulário de cadastro de propriedade

--- lib/pages/cadaster_property_page.dart
+++ lib/pages/cadaster_property_page.dart
@@ -8,8 +8,186 @@ class CadasterProperty_page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.black,
+        width: 430,
+          height: 865,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(color: Colors.white),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 48.78,
+                top: 596.04,
+                child: Container(
+                  width: 319.04,
+                  height: 36.52,
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 0.50, color: Color(0xFFFF0101)),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 0,
+                top: 0,
+                child: Container(
+                  width: 430,
+                  height: 113.75,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF1D3B46),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(6),
+                        bottomRight: Radius.circular(6),
+                      ),
+                    ),
+                    shadows: [
+                      BoxShadow(
+                        color: Color(0x3F000000),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 121.94,
+                top: 56.88,
+                child: Text(
+                  'Cadastro de Propriedade',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w600,
+                    height: 0,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 176.31,
+                top: 159.46,
+                child: Text(
+                  'HARVESTID',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w500,
+                    height: 0,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 59.92,
+                top: 234.57,
+                child: Text(
+                  'CEP',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 64.48,
+                top: 302.55,
+                child: Text(
+                  'Endereço',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 65.01,
+                top: 375.70,
+                child: Text(
+                  'Bairro',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 67.42,
+                top: 448.86,
+                child: Text(
+                  'Número',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 51.71,
+                top: 598.30,
+                child: SizedBox(
+                  width: 329.51,
+                  child: Text(
+                    'Assinale abaixo a(s) cultura (as) presentes na sua propriedade',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.w400,
+                      height: 0,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 59.92,
+                top: 666.32,
+                child: Container(
+                  width: 15,
+                  height: 15,
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Color(0xFF1D3B46)),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 85.02,
+                top: 666.32,
+                child: Text(
+                  'Tomate',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+              ),
+            ],
+          ),
       ),
-    );
+        );
+      
   }
 }
+        
+       

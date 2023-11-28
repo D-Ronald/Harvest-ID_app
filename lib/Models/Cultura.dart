6af332001fc9ecff7a899523176545b4c1195eb5
D-Ronald <danielronald21@gmail.com>
2023-11-28 11:32:45 -0300

    Revert "Funcionalidade: -Captura dos dados"
    This reverts commit c979f8a31c1ec5fc0d31e319f2f5c675e1eaf2bb.

--- lib/Models/Cultura.dart
+++ lib/Models/Cultura.dart
@@ -2,18 +2,10 @@ class Cultura {
   String nome;
   String info;
   String icone;
-  
 
   Cultura({
-    required this.info,
-    required this.nome,
-    required this.icone,
+  required this.info,
+  required this.nome,
+  required this.icone,
   });
-
-  factory Cultura.fromMap(Map<String, dynamic> map){
-    return Cultura(info: 'info',
-     nome: map['nome'],
-     icone: map['icone'],
-     );
-  }
 }

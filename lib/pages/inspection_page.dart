e3f4be5fe3476bd73901ecbe9be68d6144050154
raynayara <raysllannayara10@gmail.com>
2023-12-10 00:14:29 -0300

    Insperção Individual - dashboard

--- lib/pages/inspection_page.dart
+++ lib/pages/inspection_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class InspectionPage extends StatefulWidget {
-  const InspectionPage({super.key});
+  const InspectionPage({Key? key}) : super(key: key);
 
   @override
-  State<InspectionPage> createState() => _InspectionPageState();
+  _InspectionPageState createState() => _InspectionPageState();
 }
 
 class _InspectionPageState extends State<InspectionPage> {
@@ -12,19 +12,14 @@ class _InspectionPageState extends State<InspectionPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Column(
-          children: [
-            Text(
-              'Insperção 1',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontFamily: 'Inter',
-                fontWeight: FontWeight.w700,
-                height: 0,
-              ),
-            ),
-          ],
+        title: const Text(
+          'INSPERÇÃO 1',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w700,
+          ),
         ),
         centerTitle: true,
         backgroundColor: const Color.fromRGBO(19, 56, 58, 1),
@@ -37,6 +32,77 @@ class _InspectionPageState extends State<InspectionPage> {
             bottomLeft: Radius.circular(8),
             bottomRight: Radius.circular(8),
           ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 20, 20, 1),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Propriedade: ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Cultura:  ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Hectares: ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Data da inspeção: ',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: Center(
+                    child: Transform.rotate(
+                      angle: -3.14,
+                      child: Container(
+                        width: 354,
+                        decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              width: 1,
+                              strokeAlign: BorderSide.strokeAlignCenter,
+                              color: Colors.black.withOpacity(0.25),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );

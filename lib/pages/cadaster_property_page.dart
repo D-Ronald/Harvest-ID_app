6d1c39af8113649c09851e2c8860d79ee9b5debe
raynayara <raysllannayara10@gmail.com>
2023-12-11 23:55:10 -0300

    Ajuste fino

--- lib/pages/cadaster_property_page.dart
+++ lib/pages/cadaster_property_page.dart
@@ -6,13 +6,9 @@ import 'package:debug_no_cell/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:debug_no_cell/utils/base.dart';
 
-
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();  
-    
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 }
-
 
 class CadasterProperty_page extends StatefulWidget {
   @override
@@ -29,8 +25,6 @@ class _CadasterPropertyPageState extends State<CadasterProperty_page> {
   String? selectedCity;
   bool isChecked = false;
 
-
-
   void _exibirDialogoCadastroSucesso(BuildContext context) {
     showDialog(
       context: context,
@@ -42,7 +36,7 @@ class _CadasterPropertyPageState extends State<CadasterProperty_page> {
             TextButton(
               child: const Text('OK'),
               onPressed: () {
-                Navigator.of(context).pop(); // 
+                Navigator.of(context).pop(); //
               },
             ),
           ],
@@ -50,115 +44,166 @@ class _CadasterPropertyPageState extends State<CadasterProperty_page> {
       },
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Cadastre a Propriedade')),
-        body: SingleChildScrollView(
-            child: Column(children: [
-      genericBigImage(
-          context: context,
-          src: "assets/images/NameGray.png",
-          heightPercentage: 5,
-          widthPercentage: 50),
-      spacing(context, 3),
-      genericTextFormPassword(
-          context: context,
-          controller: _propertyNameController,
-          labeltext: "Nome da propriedade",
-          labelColor: dark_gray_base,
-          heightPercentage: 8,
-          padding: 10,
-          color: black_base,
-          backgroundColor: ligth_gray_base,
-          borderRadius: 20),
-      spacing(context, 3),
-      genericTextFormPassword(
-          context: context,
-          controller: _propertySizeController,
-          labeltext: "Tamanho da propriedade em hectares",
-          labelColor: dark_gray_base,
-          heightPercentage: 8,
-          padding: 10,
-          color: black_base,
-          backgroundColor: ligth_gray_base,
-          borderRadius: 20),
-      spacing(context, 3),
-          const SizedBox(
+      appBar: AppBar(
+        title: const Column(
+          children: [
+            Text(
+              'CADASTRO DE PROPRIEDADES',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w700,
+                height: 0,
+              ),
+            )
+          ],
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(19, 56, 58, 1),
+        shadowColor: const Color.fromRGBO(19, 56, 60, 38),
+        elevation: 10,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(1),
+            topRight: Radius.circular(1),
+            bottomLeft: Radius.circular(8),
+            bottomRight: Radius.circular(8),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            spacing(context, 2),
+            genericBigImage(
+                context: context,
+                src: "assets/images/NameGray.png",
+                heightPercentage: 4,
+                widthPercentage: 30),
+            spacing(context, 2),
+            Center(
+              child: Transform.rotate(
+                angle: -3.14,
+                child: Container(
+                  width: 354,
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        width: 1,
+                        strokeAlign: BorderSide.strokeAlignCenter,
+                        color: Colors.black.withOpacity(0.25),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            spacing(context, 4),
+            genericTextFormPassword(
+                context: context,
+                controller: _propertyNameController,
+                labeltext: "Nome da propriedade",
+                labelColor: dark_gray_base,
+                heightPercentage: 8,
+                padding: 25,
+                color: black_base,
+                backgroundColor: ligth_gray_base,
+                borderRadius: 10),
+            spacing(context, 1),
+            genericTextFormPassword(
+                context: context,
+                controller: _propertySizeController,
+                labeltext: "Tamanho da propriedade em hectares",
+                labelColor: dark_gray_base,
+                heightPercentage: 8,
+                padding: 25,
+                color: black_base,
+                backgroundColor: ligth_gray_base,
+                borderRadius: 10),
+            spacing(context, 3),
+            const SizedBox(
               width: 357,
               height: 31,
               child: Text(
                 'LOCALIZAÇÃO',
-              textAlign: TextAlign.center,
-              style: TextStyle(
+                textAlign: TextAlign.center,
+                style: TextStyle(
                   color: Colors.black,
-                  fontSize: 13,
+                  fontSize: 16,
                   fontFamily: 'Raleway',
                   fontWeight: FontWeight.w400,
                   height: 0,
-        ),
-    ),
-),
-      spacing(context, 3),
-      Container(
-        padding: const EdgeInsets.all(10), 
-        child: CSCPicker(
-            onCountryChanged: (country){
-              setState(() {
-                selectedCountry = country;
-                });
-
-            },
-            onCityChanged: (city){
-              setState(() {
-                selectedCity = city;
-                });
-            },
-            onStateChanged: (state){
-              setState(() {
-                selectedState = state;
-                });
-            },
-            dropdownDialogRadius: 20,
-            searchBarRadius: 20,
-            countrySearchPlaceholder: "País",
-            stateSearchPlaceholder: "Estado",
-            citySearchPlaceholder: "Cidade",
-            countryDropdownLabel: "País",
-            cityDropdownLabel: "Cidade",
-            stateDropdownLabel: "Estado",
-  ),
-),
-      spacing(context, 3),
-      genericTextFormPassword(
-          context: context,
-          controller: _addressController,
-          labeltext: "Endereço",
-          labelColor: dark_gray_base,
-          heightPercentage: 8,
-          padding: 10,
-          color: black_base,
-          backgroundColor: ligth_gray_base,
-          borderRadius: 10),
-
-      spacing(context, 5),
-      const SizedBox(
-        width: 406,
-        height: 50,
-        child: Text(
-          'Quais destas culturas estão presentes em sua plantação?',
-          style: TextStyle(
-            color: Color.fromRGBO(62, 68, 69, 1),
-            fontSize: 16,
-            fontFamily: 'Raleway',
-            fontWeight: FontWeight.w400,
-            height: 0,
-),
-),
-),
-      Row(
-                children: [
-                  Checkbox(
+                ),
+              ),
+            ),
+            spacing(context, 2),
+            Container(
+              padding: const EdgeInsets.all(5),
+              child: SizedBox(
+                width: 360,
+                child: CSCPicker(
+                  onCountryChanged: (country) {
+                    setState(() {
+                      selectedCountry = country;
+                    });
+                  },
+                  onCityChanged: (city) {
+                    setState(() {
+                      selectedCity = city;
+                    });
+                  },
+                  onStateChanged: (state) {
+                    setState(() {
+                      selectedState = state;
+                    });
+                  },
+                  dropdownDialogRadius: 20,
+                  searchBarRadius: 20,
+                  countrySearchPlaceholder: "País",
+                  stateSearchPlaceholder: "Estado",
+                  citySearchPlaceholder: "Cidade",
+                  countryDropdownLabel: "País",
+                  cityDropdownLabel: "Cidade",
+                  stateDropdownLabel: "Estado",
+                ),
+              ),
+            ),
+            spacing(context, 2),
+            genericTextFormPassword(
+                context: context,
+                controller: _addressController,
+                labeltext: "Endereço",
+                labelColor: dark_gray_base,
+                heightPercentage: 8,
+                padding: 25,
+                color: black_base,
+                backgroundColor: ligth_gray_base,
+                borderRadius: 10),
+            spacing(context, 2),
+            const SizedBox(
+              width: 350,
+              height: 50,
+              child: Text(
+                'Quais destas culturas estão presentes em sua plantação?',
+                style: TextStyle(
+                  color: Color.fromRGBO(62, 68, 69, 1),
+                  fontSize: 16,
+                  fontFamily: 'Raleway',
+                  fontWeight: FontWeight.w400,
+                  height: 0,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16.0),
+                  child: Checkbox(
                     value: isChecked,
                     onChanged: (bool? checked) {
                       if (checked != null) {
@@ -169,27 +214,29 @@ class _CadasterPropertyPageState extends State<CadasterProperty_page> {
                     },
                     activeColor: const Color(0x3F000000),
                   ),
-                  const Text('Tomateiro'),
-                ],
-              ),
-      spacing(context, 5),
-        genericButton(
-          context,
-          darkGreenBase,
-          white_base,
-          "Cadastrar propriedade",
-          3,
-          20,
-          (){
-            _autenthicationService.cadasterProperty(context: context,
-             propertyName: _propertyNameController.text, 
-             propertySize: _propertySizeController.text, 
-             address: _addressController.text, 
-             selectedCountry: selectedCountry, 
-             selectedState: selectedState, 
-             selectedCity: selectedCity, 
-             isChecked: isChecked);
-          },
+                ),
+                const Text('Tomateiro'),
+              ],
+            ),
+            spacing(context, 3),
+            genericButton(
+              context,
+              darkGreenBase,
+              white_base,
+              "Cadastrar propriedade",
+              6,
+              25,
+              () {
+                _autenthicationService.cadasterProperty(
+                    context: context,
+                    propertyName: _propertyNameController.text,
+                    propertySize: _propertySizeController.text,
+                    address: _addressController.text,
+                    selectedCountry: selectedCountry,
+                    selectedState: selectedState,
+                    selectedCity: selectedCity,
+                    isChecked: isChecked);
+              },
             ),
           ],
         ),

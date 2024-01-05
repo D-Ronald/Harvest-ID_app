3757e96c39e5a563317073debc737d31d8226d29
Rayslla Nayara <raysllannayara10@gmail.com>
2024-01-05 11:12:34 -0300

    save das inspeções em uma listView.separated

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,5 +1,5 @@
+import 'package:debug_no_cell/Repositories/Culture_repository.dart';
 import 'package:debug_no_cell/pages/inspection_page.dart';
-import 'package:debug_no_cell/utils/base.dart';
 import 'package:flutter/material.dart';
 
 class DashboardPage extends StatefulWidget {
@@ -10,16 +10,17 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-
   void navigateToAnotherPage(BuildContext context) {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => const InspectionPage()),
     );
   }
-  
+
   @override
   Widget build(BuildContext context) {
+    final tabela = CultureRepository.tabela;
+
     return Scaffold(
       appBar: AppBar(
         title: const Column(
@@ -62,73 +63,43 @@ class _DashboardPageState extends State<DashboardPage> {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            spacing(context, 10),
-            InkWell(
-              onTap: () {
-                navigateToAnotherPage(context);
-              },
-              child: Ink(
-                child: Container(
-                  width: 350,
-                  height: 60,
-                  decoration: ShapeDecoration(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      side: const BorderSide(width: 1, color: Color(0xFF13383A)),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Inspeção salva 1',
-                          style: TextStyle(
-                            color: Color(0xFF13383A),
-                            fontSize: 18,
-                            fontFamily: 'Inter',
-                            fontWeight: FontWeight.w600,
-                            height: 0,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 90.0),
-                          child: GestureDetector(
-                            behavior: HitTestBehavior.translucent,
-                            onTap: () {
-                              // Lógica ao tocar no ícone
-                            },
-                            child: Ink(
-                              decoration: ShapeDecoration(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                ),
-                              ),
-                              child: SizedBox(
-                                width: width(context, 24), // Ajuste conforme necessário
-                                height: width(context, 24), // Ajuste conforme necessário
-                                child: const Icon(
-                                  Icons.edit,
-                                  color: Color.fromRGBO(19, 56, 58, 1),
-                                  size: 24.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int culture) {
+          return Container(
+            width: 350,
+            height: 60,
+            decoration: ShapeDecoration(
+              color: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(width: 1, color: Color(0xFF13383A)),
+                borderRadius: BorderRadius.circular(10),
               ),
             ),
-          ],
-        ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    tabela[culture].identificador,
+                    style: const TextStyle(
+                      color: Color(0xFF13383A),
+                      fontSize: 18,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                      width: 220), // Espaçamento entre o texto e o ícone
+                  tabela[culture].icone, // Adiciona o ícone ao Row
+                ],
+              ),
+            ),
+          );
+        },
+        padding: const EdgeInsets.all(15),
+        separatorBuilder: (_, __) => const Divider(),
+        itemCount: tabela.length,
       ),
     );
   }

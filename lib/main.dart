7696d8f36f348f0b2bfd5b4aa99d10b1406a472e
Rayslla Nayara <raysllannayara10@gmail.com>
2024-02-20 10:15:14 -0300

    junção da feature/dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:debug_no_cell/pages/view_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(

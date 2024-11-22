dede4754699edcd94c2b376a56b5f1d7fac4bdae
raynayara <raysllannayara10@gmail.com>
2024-11-21 21:07:01 -0300

    update send and dialogs

--- lib/pages/generics_dialogs.dart
+++ lib/pages/generics_dialogs.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:debug_no_cell/utils/base.dart';
 import 'package:debug_no_cell/utils/routes.dart';
 import 'package:flutter/material.dart';
@@ -29,4 +31,40 @@ class Dialog {
       }
     );
   }
+
+  static void imageDialog({
+  required BuildContext context,
+  required String title,
+  required Uint8List imageBytes, // Pode ser um File ou Uint8List
+  Color? color,
+}) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title, style: TextStyle(color: Colors.black)),
+        content: Column(
+          mainAxisSize: MainAxisSize.min, // Tamanho do conteúdo
+          children: [
+            // Exibindo a imagem recebida (em bytes ou File)
+            Image.memory(imageBytes),  // Para exibir os bytes da imagem
+          ],
+        ),
+        actions: <Widget>[
+          genericButton(
+            context,
+            color,
+            whiteBase,
+            "OK",
+            4,
+            8,
+            () {
+              Navigator.of(context).pop();
+            },
+          )
+        ],
+      );
+    },
+  );
+}
 }
--- lib/services/send.dart
+++ lib/services/send.dart
@@ -30,21 +30,11 @@ class SendImage {
     }
   }
 
-  string(String predict) {
-    String aux = "";
-    for (int i = 12; i < predict.length - 3; i++) {
-      aux = aux + predict[i];
-    }
-    return aux;
-  }
-
   Future<void> sendImage(context) async {
-    var url = Uri.parse('hhttps://c8c4-34-125-133-108.ngrok-free.app/predict');
+    var url = Uri.parse('https://79b1-35-245-146-92.ngrok-free.app/predict');
     var request = http.MultipartRequest('POST', url);
     request.files
-        .add(await http.MultipartFile.fromPath('image', treatArchive(file),));
-    request.files.add(await http.MultipartFile.fromPath(
-        'id', AutenthicationService().user!.uid));
+        .add(await http.MultipartFile.fromPath('image', treatArchive(file)));
     var token;
     request.headers.addAll({
       'Content-Type': 'multipart/form-data',
@@ -53,7 +43,6 @@ class SendImage {
     try {
       var response = await request.send();
       var responseString = await response.stream.bytesToString();
-      responseString = string(responseString);
       print(responseString);
       if (responseString == "no plants") {
         Dialog.dialog(
